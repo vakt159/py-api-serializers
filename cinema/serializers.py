bc35732c12c2430d01d162dcc30fc3a7e256cbc7
Andrii Yermolenko <vakt159@gmail.com>
2025-12-29 21:21:42 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -38,7 +38,7 @@ class ActorSerializer(serializers.ModelSerializer):
         model = Actor
         fields = ("id", "first_name", "last_name", "full_name")
 
-    def get_full_name(self, obj) -> str:
+    def get_full_name(self, obj: Actor) -> str:
         return f"{obj.first_name} {obj.last_name}"
 
 
